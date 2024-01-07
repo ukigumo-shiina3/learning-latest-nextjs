862ad400d8e931f2cd2efddc8af02632764f22dc
ukigumo-shiina3 <daisukeyao@outlook.jp>
2024-01-07 10:33:29 +0900

    ️️️️️♻️  RL検索パラメータの検索状態管理をuseQueryStateで置き換え

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,21 +2,23 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useQueryState } from 'nuqs';
 
 export default function Search({ placeholder }: { placeholder: string }) {
-  const searchParams = useSearchParams();
-  const pathname = usePathname();
-  const { replace } = useRouter();
+  const [query, setQuery] = useQueryState('query');
+  // const searchParams = useSearchParams();
+  // const pathname = usePathname();
+  // const { replace } = useRouter();
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
-    } else {
-      params.delete('query');
-    }
-    replace(`${pathname}?${params.toString()}`);
-  }
+  // function handleSearch(term: string) {
+  //   const params = new URLSearchParams(searchParams);
+  //   if (term) {
+  //     params.set('query', term);
+  //   } else {
+  //     params.delete('query');
+  //   }
+  //   replace(`${pathname}?${params.toString()}`);
+  // }
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -26,10 +28,12 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
-        onChange={(e) => {
-          handleSearch(e.target.value);
-        }}
-        defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => setQuery(e.target.value)}
+        value={query || ''}
+        // onChange={(e) => {
+        //   handleSearch(e.target.value);
+        // }}
+        /* defaultValue={searchParams.get('query')?.toString()} */
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
